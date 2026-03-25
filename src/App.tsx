86661e40664c885f58c5b1ab8d85d3658663044f
theolderera <ahmadhayotov2511@gmail.com>
2026-03-25 10:20:41 +0500

    230326

--- src/App.tsx
+++ src/App.tsx
@@ -1,169 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { addData, deleteData, editData, getData } from "./counter/counterSlice";
-import { Modal, Table, Button, Input, Select, Space, Tag } from "antd";
-
-const { Option } = Select;
+import React, { lazy, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+const contact = lazy(() => import("./components/Contact"));
 
 const App = () => {
-  const [idx, setIdx] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const showModal = (e) => {
-    if (e) {
-      setIdx(e);
-      reset({
-        name: e.name,
-        age: e.age,
-        status: e.status,
-      });
-    } else {
-      setIdx(null);
-      reset({
-        name: "",
-        age: "",
-        status: "",
-      });
-    }
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  interface IData {
-    name: string;
-    age: number;
-    status: boolean;
-    id: number;
-  }
-
-  const dispatch = useDispatch();
-  const { data } = useSelector((state: any) => state.counter);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
-
-  const Submit = (value) => {
-    if (idx) {
-      dispatch(editData({ id: idx.id, updatedUser: value }));
-      handleCancel();
-    } else {
-      dispatch(addData(value));
-      handleCancel();
-    }
-  };
-
-  useEffect(() => {
-    dispatch(getData());
-  }, []);
-
-  const columns = [
-    {
-      title: "Image",
-      render: () => (
-        <img
-          style={{ width: 60, borderRadius: 8 }}
-          src="https://img.freepik.com/free-photo/young-male-posing-isolated-against-blank-studio-wall_273609-12356.jpg?semt=ais_hybrid&w=740&q=80"
-        />
-      ),
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-    },
-    {
-      title: "Status",
-      render: (e) => (
-        <Tag color={e.status ? "green" : "red"}>
-          {e.status ? "Active" : "Inactive"}
-        </Tag>
-      ),
-    },
-    {
-      title: "Actions",
-      render: (e) => (
-        <Space>
-          <Button onClick={() => showModal(e)} type="primary">
-            Edit
-          </Button>
-          <Button danger onClick={() => dispatch(deleteData(e.id))}>
-            Delete
-          </Button>
-        </Space>
-      ),
-    },
-  ];
+  const swiperRef = useRef<any>(null); // ref барои идора кардани свайпер
 
   return (
-    <div style={{ padding: 20 }}>
-      <Space style={{ marginBottom: 20 }}>
-        <Button type="primary" onClick={() => showModal(null)}>
-          ADD
-        </Button>
-        <Input
-          placeholder="Search"
-          onChange={(e) => dispatch(getData({ search: e.target.value }))}
-        />
-        <Select
-          defaultValue="all"
-          style={{ width: 120 }}
-          onChange={(value) => dispatch(getData({ select: value }))}
-        >
-          <Option value="all">All</Option>
-          <Option value="true">Active</Option>
-          <Option value="false">Inactive</Option>
-        </Select>
-      </Space>
-
-      <Table
-        dataSource={data}
-        columns={columns}
-        rowKey="id"
-        pagination={false}
-      />
-
-      <Modal
-        title="Basic Modal"
-        open={isModalOpen}
-        onCancel={handleCancel}
-        footer={null}
+    <div style={{ width: "600px", margin: "0 auto" }}>
+      <Swiper
+        onSwiper={(swiper) => (swiperRef.current = swiper)} // гирифтани объект свайпер
+        slidesPerView={3} // чанд слайд намоиш дода шавад
+        spaceBetween={20} // фосилаи байни слайдҳо
       >
-        <form onSubmit={handleSubmit(Submit)}>
-          <Input placeholder="Name" {...register("name")} />
-          <Input
-            style={{ marginTop: 10 }}
-            placeholder="Age"
-            type="number"
-            {...register("age")}
-          />
-          <Select
-            style={{ width: "100%", marginTop: 10 }}
-            {...register("status")}
-          >
-            <Option value="true">Active</Option>
-            <Option value="false">Inactive</Option>
-          </Select>
-          <Button
-            style={{ marginTop: 15 }}
-            type="primary"
-            htmlType="submit"
-            block
-          >
-            Submit
-          </Button>
-        </form>
-      </Modal>
+        <SwiperSlide style={{ background: "#ff9999", height: "150px" }}>
+          Slide 1
+        </SwiperSlide>
+        <SwiperSlide style={{ background: "#99ff99", height: "150px" }}>
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide style={{ background: "#9999ff", height: "150px" }}>
+          Slide 3
+        </SwiperSlide>
+        <SwiperSlide style={{ background: "#ffcc99", height: "150px" }}>
+          Slide 4
+        </SwiperSlide>
+        <SwiperSlide style={{ background: "#cc99ff", height: "150px" }}>
+          Slide 5
+        </SwiperSlide>
+      </Swiper>
+
+      {/* Кнопкаҳои ҳаракат */}
+      <div style={{ marginTop: "20px", textAlign: "center" }}>
+        <button
+          onClick={() => swiperRef.current?.slidePrev()} // ба слайди қаблӣ ҳаракат
+          style={{ marginRight: "10px" }}
+        >
+          ← Prev
+        </button>
+        <button onClick={() => swiperRef.current?.slideNext()}>Next →</button>{" "}
+        {/* ба слайди навбатӣ ҳаракат */}
+      </div>
     </div>
   );
 };
